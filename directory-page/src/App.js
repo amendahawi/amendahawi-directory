0463d7e06cf7f54a2cbdc857e8a2f116d2160a63
Abdul Mendahawi <amendahawi@outlook.com>
2024-02-26 19:20:44 -0500

    inital directory page setup

---
+++ directory-page/src/App.js
@@ -0,0 +1,138 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* HEADER */}
+        <p>Abdul Mendahawi</p>
+        <p>am13948@nyu.edu</p>
+        <br></br>
+
+        {/* ABOUT */}
+        <p className="secondary_text">Heyy,</p>
+        <p>
+          I'm a Computer Science student at NYU. I have developed several games
+          (200 million+ play sessions), created a few websites, and programmed a
+          few things.
+        </p>
+        <p>Feel free to explore and / or reach out to me.</p>
+        <br></br>
+
+        {/* INTERESTS */}
+        <p className="secondary_text">Interests</p>
+        <ul>
+          <li>Virtual / Augmented Reality</li>
+          <li>Game Development</li>
+          <li>Computer Science</li>
+          <li>Machine Learning</li>
+          <li>F1 Racing</li>
+        </ul>
+        <br></br>
+
+        {/* PROJECTS */}
+        <p className="secondary_text">Prev. Projects</p>
+        <ul>
+          <li>
+            <a
+              href="https://yur.energy/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              IRC | Data Management Intern
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://yur.energy/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              YUR | Software Engineering Intern
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://yur.energy/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              RoVille | Game Developer
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://yur.energy/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Project Tactical | Lead Game Developer
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://yur.energy/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Millitant | Lead Game Developer
+            </a>
+          </li>
+        </ul>
+        <br></br>
+
+        {/* LINKS */}
+        <p className="secondary_text">Links</p>
+        <ul>
+          <li>
+            <a
+              href="https://twitter.com/abdulmendahawi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter | Software Engineering
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/tickyotacky"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter | Game Development
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/abdulmend/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/amendahawi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://open.spotify.com/user/kzept8nk8x88zlpl52vpxn3cg"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Spotify
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
